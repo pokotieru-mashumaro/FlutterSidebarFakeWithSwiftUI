12f50c4f2c5fa3e42c9fdb953f520508de73fc53
pokotieru-mashumaro <k827umeboshi@gmail.com>
2023-06-03 04:43:49 +0900

    Initial Commit

---
+++ FlutterSidebarFakeWithSwiftUI/FlutterSidebarFakeWithSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  FlutterSidebarFakeWithSwiftUIApp.swift
+//  FlutterSidebarFakeWithSwiftUI
+//
+//  Created by 小松虎太郎 on 2023/06/03.
+//
+
+import SwiftUI
+
+@main
+struct FlutterSidebarFakeWithSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
